2a86de69b70798bd1f5ed2db269243fd56196d66
na3la <tm.sc@protonmail.com>
2020-10-16 12:31:16 -0700

    updated mvfunc

--- mlvtk/ModelVFunc.py
+++ mlvtk/ModelVFunc.py
@@ -8,14 +8,14 @@ import tensorflow.python as tfp
 
 
 class ModelVFunc(Mlag, tfp.keras.engine.functional.Functional):
-    def __init__(self, model, msaver_path="vwd"):
+    def __init__(self, model, msaver_path="vwd", verbose=1):
         """
         Args:
             model: tensorflow.python.keras.engine.functional.Functional instance
             msaver_path: string containing path of directory to save model
             checkpoints in
         """
-        Mlag.__init__(self, model, msaver_path)
+        Mlag.__init__(self, model, msaver_path, verbose)
         tfp.keras.engine.functional.Functional.__init__(
             self, model.inputs, model.outputs, model.name, model.trainable
         )
